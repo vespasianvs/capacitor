95267a991dff9c7a26cf86a9e288489a25ffbb34
Sean Bannigan <bannigan@meetmaestro.com>
2020-01-08 12:49:18 -0500

    feat(android): allow plugins to override navigation

--- android/capacitor/src/main/java/com/getcapacitor/Plugin.java
+++ android/capacitor/src/main/java/com/getcapacitor/Plugin.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -531,6 +532,19 @@ public class Plugin {
    * Handle onStop
    */
   protected void handleOnStop() {}
+
+  /**
+   * Hook for blocking the launching of Intents by the Capacitor application.
+   *
+   * This will be called when the WebView will not navigate to a page, but
+   * could launch an intent to handle the URL. Return false to block this: if
+   * any plugin returns false, Capacitor will block the navigation. If all
+   * plugins return null, the default policy will be enforced. If at least one
+   * plugin returns true, and no plugins return false, then the URL will be
+   * opened in the WebView.
+   */
+  @SuppressWarnings("unused")
+  public Boolean shouldOpenExternalUrl(Uri url) { return null; }
 
   /**
    * Start a new Activity.
